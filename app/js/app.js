1674aa9c8404950c6a324fba7846d7871194181d
yangjunjun <ggiiss@qq.com>
2016-01-11 17:43:02 +0800

    Routing & Multiple Views

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,14 @@
-new Vue({
-  el: 'body',
-  data: {
-    query: '',
-    orderProp: 'age',    
-    phones: []
+var App = Vue.extend({});
+var router = new VueRouter();
+
+var phoneList = Vue.extend({
+  template: '#phones-list',
+  data: function(){
+    return {
+      query: '',
+      orderProp: 'age',
+      phones:[]      
+    }
   },
   created: function(){
     var self = this;
@@ -11,4 +16,28 @@ new Vue({
       self.phones = data;
     });
   }  
+});
+
+var phoneDetail = Vue.extend({
+  template: '#phones-detail',
+  data: function(){
+    return {     
+    }
+  }
 })
+
+router.map({
+  '*': {
+    component: phoneList
+  },  
+  '/phones': {
+    name: 'list',
+    component: phoneList
+  },
+  '/phones/:phoneId': { 
+    name: 'detail',
+    component:phoneDetail
+  }
+})
+
+router.start(App, 'body');
