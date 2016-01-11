d24ce59ba56b1a88569fa3b958c4d1bc4c8769cc
yangjunjun <ggiiss@qq.com>
2016-01-11 17:03:28 +0800

    Vue Templates

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,15 @@
 new Vue({
-  el:'body'
-});
+  el: 'body',
+  data: {
+    phones: [{
+      'name': 'Nexus S',
+      'snippet': 'Fast just got faster with Nexus S.'
+    }, {
+      'name': 'Motorola XOOM™ with Wi-Fi',
+      'snippet': 'The Next, Next Generation tablet.'
+    }, {
+      'name': 'MOTOROLA XOOM™',
+      'snippet': 'The Next, Next Generation tablet.'
+    }]
+  }
+})
